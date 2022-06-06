1d4c603d933ac56ea7caf7e1eeb4207ed0b424ed
marcivallario <68357009+marcivallario@users.noreply.github.com>
2022-06-06 02:19:53 -0400

    Update project links

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,17 +1,18 @@
 import "../styles/projectcard.css"
 
-function ProjectCard({ img, title, description, github, demo}) {
+function ProjectCard({ img, title, description, github, demo, liveSite}) {
+    // console.log('liveSite: ', liveSite)
     return (
         <div className="card">
             <img className="card-img-top" src={img} alt="Project Overview" title={title} />
             <div className="card-body">
-                <h5 class="card-title">{title}</h5>
+                <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
             </div>
-            <div class="card-footer">
-                <button class="project-links"><a href={github} class="card-link">Github</a></button>
-                <button class="project-links"><a href={demo} class="card-link">Video Demo</a></button>
-                <button class="project-links"><a href="" class="card-link">Live Site</a></button>
+            <div className="card-footer">
+                <button className="project-links"><a href={github} target="_blank" className="card-link">Github</a></button>
+                <button className="project-links"><a href={demo} target="_blank" className="card-link">Video Demo</a></button>
+                {liveSite ? <button className="project-links"><a href={liveSite} target="_blank" className="card-link">Live Site</a></button> : null}
             </div>
         </div>
     )
--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -9,22 +9,25 @@ function ProjectsContainer() {
             title: "TrekCheck",
             description: "TrekCheck is an app built by a former travel organizer, for travel organizers. Here, organizers can track arrangements made for employees and store traveler profiles for easy access.",
             technologies: ["React", "Ruby on Rails", "Redux"],
-            demo: "https://www.youtube.com/",
-            github: "https://github.com/marcivallario/trekcheck-co"
+            demo: "https://www.youtube.com/watch?v=C608_YnMq80&ab_channel=MarciVallario",
+            github: "https://github.com/marcivallario/trekcheck-co", 
+            liveSite: "https://trekcheck.herokuapp.com/"
         }, 
         {
             img: require('../assets/images/projects/shapeup.png'),
             title: "Shape Up",
             description: "A workout generator where users can randomly generate and save exercises for a complete full body workout.",
-            demo: "https://www.youtube.com/",
-            github: "https://github.com/marcivallario/shapeup"
+            demo: "https://www.youtube.com/watch?v=srVEAH1MKKs&ab_channel=MarciVallario",
+            github: "https://github.com/marcivallario/shapeup",
+            liveSite: 'https://shape-up-2022.herokuapp.com/'
         }, 
         {
             img: require('../assets/images/projects/zest.png'),
             title: "Zest",
             description: "A recipe curator application where users can filter through and browse cuisine-specific dishes.",
-            demo: "https://www.youtube.com/",
-            github: "https://github.com/marcivallario/zest"
+            demo: "https://www.youtube.com/watch?v=I_gp56_r5LQ&ab_channel=MarciVallario",
+            github: "https://github.com/marcivallario/zest",
+            liveSite: ''
         }
     ]
 
@@ -34,21 +37,23 @@ function ProjectsContainer() {
         title={project.title} 
         description={project.description} 
         demo={project.demo}
-        github={project.github}/>)
+        github={project.github}
+        liveSite={project.liveSite}/>)
 
     return (
-        <section id="projects" class="container-fluid">
-            <div class="row" id="card-carousel">
+        <section id="projects" className="container-fluid">
+            <div className="row" id="card-carousel">
                 <Carousel slides={slides} autoplay={true} interval={5000} />
             </div>
-            <div class="row" id="card-grid">
+            <div className="row" id="card-grid">
                 {projects.map((project, index) => <ProjectCard 
                 key={index}
                 img={project.img} 
                 title={project.title} 
                 description={project.description} 
                 demo={project.demo}
-                github={project.github}/>)}
+                github={project.github}
+                liveSite={project.liveSite}/>)}
             </div>
         </section>
     )
